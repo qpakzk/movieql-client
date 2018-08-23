435797b086f0a3a2af40da7992d640c6d0930cec
Sangwon Hong <qpakzk@gmail.com>
2018-08-23 20:21:53 +0900

    Complete MovieQL Client

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -2,7 +2,7 @@ import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
 
 injectGlobal`
-    @import url('https//fonts.googleapis.com/css?family=Moven+Pro');
+    @import url('https//fonts.googleapis.com/css?family=Moven+Pro:400,500');
     ${reset};
     body {
         font-family: 'Maven Pro', 'sans-serif';
--- src/queries.js
+++ src/queries.js
@@ -2,12 +2,32 @@ import gql from "graphql-tag";
 
 export const MOVIE_PAGE = gql`
     query {
-        movies(limit:50, rating: 1) {
+        movies(limit: 100, rating: 3) {
             id
             title
             genres
             rating
             medium_cover_image
+        }
+    }
+`;
+
+export const MOVIE_DETAILS = gql`
+    query getMovie($movieId: Int!) {
+        movie(id: $movieId) {
+            medium_cover_image
+            title
+            rating
+            description_intro
+            language
+            genres
+        }
+
+        suggestions(id: $movieId) {
+            id
+            medium_cover_image
+            title
+            rating
         }
     }
 `;
