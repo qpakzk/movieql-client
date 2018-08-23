b424723b3b3c138db886c0631850b52316453a19
Sangwon Hong <qpakzk@gmail.com>
2018-08-23 17:03:29 +0900

    Add Movie component and CSS

---
+++ src/globalStyles.js
@@ -0,0 +1,21 @@
+import { injectGlobal } from "styled-components";
+import reset from "styled-reset";
+
+injectGlobal`
+    @import url('https//fonts.googleapis.com/css?family=Moven+Pro');
+    ${reset};
+    body {
+        font-family: 'Maven Pro', 'sans-serif';
+        background-color: #ecf0f1;
+    }
+
+    a {
+        color: inherit;
+    }
+
+    main {
+        width: 80%;
+        margin: 0 auto;
+        padding: 20px;
+    }
+`
--- src/queries.js
+++ src/queries.js
@@ -2,11 +2,12 @@ import gql from "graphql-tag";
 
 export const MOVIE_PAGE = gql`
     query {
-        movies(limit:50, rating: 7) {
+        movies(limit:50, rating: 1) {
             id
             title
             genres
             rating
+            medium_cover_image
         }
     }
 `;
