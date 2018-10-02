6ef6541c0e592ca8aac4dfe1ba22fd09f7d3d10a
Heo kyungyoung <bbvch13531@gmail.com>
2018-10-02 23:11:05 +0900

    Change tab size
    Change tab size 8 to 2

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Detail from "./Detail";
 class App extends Component {
   render() {
     return <ApolloProvider client={client}>
-			<Router>
-				<main>
-					<Route exact={true} path={'/'} component={Home} />
-					<Route path={'/details/:movieId'} component={Detail} />
-				</main>
-			</Router>
-		</ApolloProvider>
+	     <Router>
+		<main>
+		  <Route exact={true} path={'/'} component={Home} />
+		  <Route path={'/details/:movieId'} component={Detail} />
+		</main>
+	     </Router>
+	   </ApolloProvider>
   }
 }
 
